fe624f7a5f1726de3832da2925be789bcd169601
alumno <alumno>
2017-04-27 19:37:00 -0300

    Creacion de la carpeta clase herencia

---
+++ claseherencia/Entidades/Alumno.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Entidades
+{
+  public  class Alumno
+    {
+      private string _nombre;
+      private string _apellido;
+      private int _dni;
+      private List<float> _notas;
+      private string _curso;
+
+
+      public Alumno(string nombre, string apellido, int dni)
+      {
+          this._nombre = nombre;
+          this._apellido = apellido;
+          this._dni = dni;
+      }
+
+
+      public Alumno(string nombre, string apellido, int dni,string curso)
+      {
+          this._nombre = nombre;
+          this._apellido = apellido;
+          this._dni = dni;
+          this._curso = curso;
+      }
+
+
+      public string Mostrar()
+      {
+          StringBuilder sb = new StringBuilder();
+
+          sb.AppendLine("Nombre" + this._nombre);
+          sb.AppendLine("Apellido" + this._apellido);
+          sb.AppendLine("Dni" + this._dni);
+          sb.AppendLine("Curso" + this._curso);
+          sb.AppendLine("Las notas son: ");
+
+          foreach (float item in _notas)
+          {
+              sb.AppendLine(item.ToString());
+          }
+
+          return sb.ToString();
+      }
+    }
+}
---
+++ claseherencia/Entidades/Obrero.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Entidades
+{
+   public class Obrero
+    {
+        private string _nombre;
+        private string _apellido;
+        private int _dni;
+        private int _legajo;
+        private float _sueldo;
+
+        public Obrero(string nombre, int dni, string apellido)
+        {
+            this._nombre = nombre;
+            this._dni = dni;
+            this._apellido = apellido;
+            
+        }
+
+        public Obrero(string nombre, int dni, string apellido,int legajo,float sueldo)
+        {
+            this._nombre = nombre;
+            this._dni = dni;
+            this._apellido = apellido;
+            this._legajo = legajo;
+            this._sueldo = sueldo;
+
+        }
+
+        public string Mostrar()
+        {
+            StringBuilder sb = new StringBuilder();
+
+            sb.AppendLine("Nombre" + this._nombre);
+            sb.AppendLine("Apellido" + this._apellido);
+            sb.AppendLine("Dni" + this._dni);
+            sb.AppendLine("Legajo" + this._legajo);
+            sb.AppendLine("Sueldo " + this._sueldo);
+
+            return sb.ToString();
+        }
+    }
+}
