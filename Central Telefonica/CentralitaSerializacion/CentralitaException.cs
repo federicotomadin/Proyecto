99edfd10b624db65ef9d92f4c482966b6db3325c
federicotomadin <federicotomadin@gmail.com>
2017-05-15 23:10:36 -0300

    Centralita Serializacion

---
+++ Central Telefonica/CentralitaSerializacion/CentralitaException.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CentralitaSerializacion
+{
+   public class CentralitaException:Exception
+    {
+        #region Atributos
+
+        private Exception _exceptionInterna;
+        private string _nombreClase;
+        private string _nombreMetodo;
+        #endregion
+
+        #region Propiedades
+
+        public Exception ExcepcionInterna
+        {
+            get { return _exceptionInterna; }
+        }
+
+        public String NombreClase
+        {
+            get { return _nombreClase; }
+        }
+
+        public String NombreMetodo
+        {
+            get { return _nombreMetodo; }
+        }
+
+        #endregion
+
+
+        #region Metodos
+
+        public CentralitaException(string mensaje, string clase, string metodo)
+            : base(mensaje)
+        {
+            this._nombreClase = clase;
+            this._nombreMetodo = metodo;
+
+        }
+
+
+        public CentralitaException(string mensaje, string clase, string metodo, Exception innerException)
+            : this(mensaje, clase, metodo)
+        {
+
+        }
+        #endregion
+    }
+}
---
+++ Central Telefonica/CentralitaSerializacion/ISerializable.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CentralitaSerializacion
+{
+   public interface ISerializable
+    {
+       public string RutaDelArchivo { get; set; }
+
+       public bool DeSerializarse();
+
+       public bool Serializarse();
+    }
+}
